7c6f59c1fdd708a4adf96abac8c261e628c0cf81
a.lashnev <lexey2006.90@mail.ru>
2024-11-19 20:48:55 +0300

    No EnableScheduling needed

--- src/main/kotlin/ru/lashnev/forwarderbackend/TelegramForwarderApplication.kt
+++ src/main/kotlin/ru/lashnev/forwarderbackend/TelegramForwarderApplication.kt
@@ -2,10 +2,8 @@ package ru.lashnev.forwarderbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
 class TelegramForwarderApplication
 
 fun main(args: Array<String>) {
