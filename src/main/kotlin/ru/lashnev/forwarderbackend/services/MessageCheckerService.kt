3ea83fe64708016dcca1724a95e54315c8ea7b2f
a.lashnev <lexey2006.90@mail.ru>
2024-12-23 05:12:00 -0500

    Use stemming instead lemmatization

--- src/main/kotlin/ru/lashnev/forwarderbackend/services/MessageCheckerService.kt
+++ src/main/kotlin/ru/lashnev/forwarderbackend/services/MessageCheckerService.kt
@@ -1,32 +1,49 @@
 package ru.lashnev.forwarderbackend.services
 
-import com.github.demidko.aot.WordformMeaning.lookupForMeanings
 import org.springframework.stereotype.Service
+import org.tartarus.snowball.ext.RussianStemmer
+import org.tartarus.snowball.ext.EnglishStemmer
 
 @Service
 class MessageCheckerService {
     fun containKeyword(message: String, keywords: Set<String>): Boolean {
         val messageLemmas = message.split(Regex("[\\s,!?.]+"))
             .filterNot { it.isEmpty() }
-            .map {
-                val wordParts = lookupForMeanings(it)
-                if (wordParts.isEmpty()) it else wordParts[0].lemma.toString()
-            }
+            .map { it.stem() }
             .map { it.lowercase() }
 
 
         keywords.forEach { keyword ->
             val keywordLemmas = keyword.split(Regex("[\\s,!?.]+"))
                 .filterNot { it.isEmpty() }
-                .map {
-                    val wordParts = lookupForMeanings(it)
-                    if (wordParts.isEmpty()) it else wordParts[0].lemma.toString()
-                }
+                .map { it.stem() }
                 .map { it.lowercase() }
             if (messageLemmas.containsAll(keywordLemmas)) {
                 return true
             }
         }
         return false
+    }
+
+    private fun String.stem(): String {
+        return if (isRussian(this)) {
+            russianStemmer.current = this
+            russianStemmer.stem()
+            russianStemmer.current
+        } else {
+            englishStemmer.current = this
+            englishStemmer.stem()
+            englishStemmer.current
+        }
+    }
+
+    private fun isRussian(message: String): Boolean {
+        val regex = Regex("[а-яА-ЯёЁ]+")
+        return regex.containsMatchIn(message)
+    }
+
+    companion object {
+        val russianStemmer = RussianStemmer()
+        val englishStemmer = EnglishStemmer()
     }
 }
