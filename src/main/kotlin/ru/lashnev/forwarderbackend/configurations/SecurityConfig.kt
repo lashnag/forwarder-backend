b625cc2b6dabb62319cf47993abc39e0584e8762
a.lashnev <lexey2006.90@mail.ru>
2025-02-01 13:57:43 +0300

    Add basic auth for controllers

---
+++ src/main/kotlin/ru/lashnev/forwarderbackend/configurations/SecurityConfig.kt
@@ -0,0 +1,19 @@
+package ru.lashnev.forwarderbackend.configurations
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { requests -> requests.anyRequest().authenticated() }.httpBasic { }
+        return http.build()
+    }
+}
--- src/test/kotlin/ru/lashnev/forwarderbackend/controllers/SubscriptionsControllerTest.kt
+++ src/test/kotlin/ru/lashnev/forwarderbackend/controllers/SubscriptionsControllerTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -25,6 +26,7 @@ class SubscriptionsControllerTest : BaseIT() {
 
     @Test
     @Sql("/sql/subscriptions.sql")
+    @WithMockUser(username = "admin")
     fun getAllSubscriptions() {
         val responseString = mockMvc.perform(
             get("/api/subscriptions").accept(MediaType.APPLICATION_JSON))
@@ -39,6 +41,7 @@ class SubscriptionsControllerTest : BaseIT() {
 
     @Test
     @Sql("/sql/subscriptions_v2.sql")
+    @WithMockUser(username = "admin")
     fun getAllSubscriptionsWithoutV2() {
         val responseString = mockMvc.perform(
             get("/api/subscriptions").accept(MediaType.APPLICATION_JSON))
@@ -49,5 +52,12 @@ class SubscriptionsControllerTest : BaseIT() {
         val setDtoTypeReference: TypeReference<Set<SubscriptionRawDto>> = object : TypeReference<Set<SubscriptionRawDto>>() {}
         val response = objectMapper.readValue(responseString, setDtoTypeReference)
         assertEquals(1, response.size)
+    }
+
+    @Test
+    fun getSubscriptionsHasAuth() {
+        mockMvc.perform(
+            get("/api/subscriptions").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().is4xxClientError)
     }
 }
